4ae258d8aab50b4f9fe74c850ed6e9ebb2abcba9
Smembe812 <psemberekajr@gmail.com>
2020-02-03 17:38:17 +0100

    .storybook & .gitignore added

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+    stories: ['../src/**/*.stories.([tj]s|mdx)'],
+    addons: [
+        '@storybook/addon-docs', 
+        '@storybook/addon-storysource',
+        'storypug'
+    ],
+}
